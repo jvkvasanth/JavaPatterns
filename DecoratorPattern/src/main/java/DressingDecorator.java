b483f46ab81adb0998cc7dde54dc7711629bca8a
jvkvasanth <jvkvasanth@gmail.com>
2021-05-02 15:54:51 +0530

    Decorator Pattern Example

--- DecoratorPattern/src/main/java/DressingDecorator.java
+++ DecoratorPattern/src/main/java/DressingDecorator.java
@@ -1,2 +1,14 @@
-public class DressingDecorator {
+public class DressingDecorator  extends  SandwichDecorator{
+
+    DressingDecorator(Sandwich sandwich){
+        super(sandwich);
+    }
+
+    public String make() {
+        return sandwich.make() + addDressing() ;
+    }
+
+    private String addDressing(){
+        return " Added Greens with added chees";
+    }
 }
--- DecoratorPattern/src/main/java/SandwichStore.java
+++ DecoratorPattern/src/main/java/SandwichStore.java
@@ -2,7 +2,7 @@ public class SandwichStore {
     public static void main(String[] args){
 
         //Get plain vannila sandwich
-        Sandwich sandwich = new MeatDecorator(new VannilaSandwich());
+        Sandwich sandwich = new DressingDecorator(new MeatDecorator(new VannilaSandwich()));
         System.out.println(sandwich.make());
     }
 }
